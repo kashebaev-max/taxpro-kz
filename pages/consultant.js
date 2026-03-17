e95aec86c542045ad8ed2568e7e659a978c09968
kashebaev-max <kashebaev@gmail.com>
2026-03-17 20:27:54 +0500

    Add files via upload

--- pages/consultant.js
+++ pages/consultant.js
@@ -96,7 +96,7 @@ export default function Consultant() {
     setLoading(true)
 
     try {
-      const res = await fetch('https://api.anthropic.com/v1/messages', {
+      const res = await fetch('/.netlify/functions/chat', {
         method:'POST',
         headers:{ 'Content-Type':'application/json' },
         body: JSON.stringify({
